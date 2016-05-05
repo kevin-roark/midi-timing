794075225bdaebf15cdcb84da3fb87c82a549e2f
Kevin Roark <ker2143@columbia.edu>
2016-05-04 21:32:14 -0400

    first version migrated from frampton

---
+++ cli.js
@@ -0,0 +1,18 @@
+
+var fs = require('fs');
+var midiTiming = require('./midi-timing');
+
+var args = process.argv.slice(2);
+if (args.length < 1) {
+  console.log('need an input midi file as first argument...');
+  return;
+}
+
+var midiFile = args[0];
+var outFile = args.length > 1 ? args[1] : 'timing.json';
+
+var midiData = fs.readFileSync(midiFile, 'binary');
+
+var timing = midiTiming(midiData);
+
+fs.writeFileSync(outFile, JSON.stringify(timing));
---
+++ midi.js
@@ -0,0 +1,106 @@
+
+var midiConverter = require('midi-converter');
+
+module.exports = function(midiSong) {
+  var jsonSong = midiConverter.midiToJson(midiSong);
+
+  var tempoMap = getTempoMap(jsonSong);
+
+  var musicTracks = getMusicTracks(jsonSong.tracks);
+
+  var timingTracks = [];
+  musicTracks.forEach(function(track) {
+    timingTracks.push(getTimingTrack(track));
+  });
+
+  return {
+    tracks: timingTracks
+  };
+
+  function getTempoMap(jsonSong) {
+    var tempos = [];
+
+    jsonSong.tracks[0].forEach(function(el) {
+      if (el.subtype === 'setTempo') {
+        tempos.push({
+          microsecondsPerBeat: el.microsecondsPerBeat,
+          time: tempos.length === 0 ? 0 : ticksToMS(el.deltaTime, tempos[tempos.length - 1].microsecondsPerBeat)
+        });
+      }
+    });
+
+    return tempos;
+  }
+
+  function getTimingTrack(track) {
+    var timedTrack = [];
+    var activeElsByNoteNumber = {};
+
+    var currentTime = 0;
+    var currentTempoIndex = 0;
+    var currentProgramNumber = 1;
+
+    for (var i = 0; i < track.length; i++) {
+      var el = track[i];
+
+      currentTime += ticksToMS(el.deltaTime, tempoMap[currentTempoIndex].microsecondsPerBeat);
+
+      if (currentTempoIndex < tempoMap.length - 1 && currentTime >= tempoMap[currentTempoIndex + 1].time) {
+        currentTempoIndex += 1;
+      }
+
+      switch (el.subtype) {
+        case 'noteOn':
+          el.__currentTime = currentTime;
+          el.__currentProgramNumber = currentProgramNumber;
+          activeElsByNoteNumber[el.noteNumber] = el;
+          break;
+
+        case 'noteOff':
+          var correspondingEl = activeElsByNoteNumber[el.noteNumber];
+          if (correspondingEl) {
+            var timeEl = {
+              time: correspondingEl.__currentTime,
+              duration: currentTime - (correspondingEl.__currentTime),
+              noteNumber: el.noteNumber,
+              velocity: correspondingEl.velocity,
+              programNumber: correspondingEl.__currentProgramNumber
+            };
+            timedTrack.push(timeEl);
+
+            delete activeElsByNoteNumber[el.noteNumber];
+          }
+          break;
+
+        case 'programChange':
+          currentProgramNumber = el.programNumber;
+          break;
+      }
+    }
+
+    return timedTrack;
+  }
+
+  function ticksToMS(ticks, microsecondsPerBeat) {
+    var beats = ticks / jsonSong.header.ticksPerBeat;
+    var microseconds = microsecondsPerBeat * beats;
+    var ms = microseconds / 1000;
+    return ms;
+  }
+};
+
+function getMusicTracks(tracks) {
+  var musicTracks = [];
+
+  tracks.forEach(function(track) {
+    for (var i = 0; i < track.length; i++) {
+      var el = track[i];
+      if (el.noteNumber) {
+        musicTracks.push(track);
+        return;
+      }
+    }
+  });
+
+  return musicTracks;
+}
